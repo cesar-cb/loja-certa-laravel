4d2f87710f4ab85876156a4e8ee3cd3a1d22b39d
Cesar Boaventura <cesar.boaventura@hotmail.com>
2016-10-06 21:21:55 +0000

    dashboard link to admin logo

--- resources/views/layouts/store.blade.php
+++ resources/views/layouts/store.blade.php
@@ -34,7 +34,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand logo-desk" href="#"> <img src="/image/logobranco.png" alt="Loja certa"></a>
+          <a class="navbar-brand logo-desk" href="/dashboard"> <img src="/image/logobranco.png" alt="Loja certa"></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right" style="float:right !important;">
